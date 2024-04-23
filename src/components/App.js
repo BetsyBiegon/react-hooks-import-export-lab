47d0a89a58026e73e2f4c7427babe6dbdf23aee1
BetsyBiegon <betsybiegon@gmail.com>
2024-04-23 14:08:25 +0300

    make changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar"; // Import NavBar component
+import Home from "./Home"; // Import Home component
+import About from "./About"; // Import About component
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar /> {/* Render NavBar component */}
+      <Home /> {/* Render Home component */}
+      <About /> {/* Render About component */}
     </div>
   );
 }
